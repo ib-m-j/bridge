7193adaea811e9807239b382cf4dde38783c2651
ib-m-j <ib.m.jorgensen@gmail.com>
2018-09-11 12:39:25 +0200

    improved tournament now it finds all lines - also included various random old files

--- tournament.py
+++ tournament.py
@@ -38,7 +38,14 @@ class Corde:
         res = res + ((self.normalized[1] - self.normalized[0])*3 + 1)*"."
         return res
 
-
+    def rotate(self, r):
+        assert 0<=r and r<self.universe, "rotation in Corde too high"
+        if self.start + r >= self.universe:
+            start = (self.start + r) % self.universe
+        else:
+            start = self.start + r
+            
+        return Corde(self.universe, start, self.length)
             
         
     def avoids(self, other):
@@ -69,6 +76,10 @@ class CordeSet:
             res = res + "\tcorde: {}\n ".format(c)
         return res
     
+    def __iter__(self):
+        for x in self.cordes:
+            yield x
+        
     def graphic(self):
         res = ""
         for x in range(self.universe):
@@ -102,15 +113,39 @@ class CordeSet:
                     overlap = overlap + 1
         return overlap
 
-    def isOrthogonal(self, other):
-        pass
+    def rotateAll(self):
+        for x in range(self.universe):
+            yield self.rotate(x)
+        
+    def maxOverlap(self, other):
+        resOverlap = 0
+        resCordeSet = []
+        for cSet in other.rotateAll():
+            #print("testing", cSet)
+            if self.overlap(cSet) > resOverlap:
+                resOverlap = self.overlap(cSet)
+                resCordeSet = cSet
+                #print("found", resOverlap)
+        assert resOverlap > 0, "overlap on incomplete sets"
+        return(resOverlap, resCordeSet)    
+
     
+    def isOrthogonal(self, other):
+        if self.maxOverlap(other)[0] == 1:
+            return True
+
+    def rotate(self, r):
+        assert 0<=r and r<self.universe, "rotate by too much"
+        newCordes = [x.rotate(r) for x in self.cordes]
+        return CordeSet(self.universe, newCordes)
             
 class CordeSets:        
     def __init__(self, n):
         self.universe = n
         self.setAllCordes()
         self.setAllExclusiveCordeSets()
+        self.setAllCordeSizes()
+        #only undirected
         #self.allExclusiveSets()
 
 
@@ -123,19 +158,56 @@ class CordeSets:
         
         return self.allCordes
 
+    def setAllCordeSizes(self):
+        #still normalized to 0.1 first
+        self.allCordeSizes = []
+        res = CordeSet(self.universe, [Corde(self.universe, 0,1)])
+        maxLength = self.universe//2
+        length = 2
+        self.addNewSizeCorde(res, length, maxLength)
+        return self.allCordeSizes
+
+    def addNewSizeCorde(self, myRes, length, maxLength):
+        #print("adding length", length)
+        for x in range(2, self.universe):
+            #print("testing corde", x, length)
+            tester = Corde(self.universe, x, length)
+            #print(myRes.graphic())
+            if myRes.hasRoom(tester):
+                if length == maxLength:
+                    self.allCordeSizes.append(myRes.clone().addCorde(tester).clone())
+                    #print("found result\n")
+                else:
+                    self.addNewSizeCorde(myRes.clone().addCorde(tester).clone(), length + 1, maxLength)
+            
+                       
+    
     def setAllExclusiveCordeSets(self):
+        #standardised first corde is always 0.1
         self.allExclusiveCordeSets = []
         all = self.allCordes
-        allTestCords = all[:]
+
+        #allTestCords = all[:]
         #for (start, cord) in enumerate(allTestCords):
         #    result = CordeSet(self.universe, [])
         #    newRes = self.addExclusiveCord(
         #self.exclusiveCords, result, allTestCords[start+1:][:])
         
-        result = CordeSet(self.universe, [])
+        all = []
+        startCorde = Corde(self.universe, 0,1)
+        for x in  self.allCordes :
+            if x != startCorde:
+                all.append(x)
+        result = CordeSet(self.universe, [startCorde])
+        allTestCords = all[:]
         newRes = self.addExclusiveCord(
             self.allExclusiveCordeSets, result, allTestCords[:])
         return self.allExclusiveCordeSets
+    
+        #result = CordeSet(self.universe, [])
+        #newRes = self.addExclusiveCord(
+        #    self.allExclusiveCordeSets, result, allTestCords[:])
+        #return self.allExclusiveCordeSets
         
 
         
@@ -162,6 +234,13 @@ class CordeSets:
                     self.addExclusiveCord(allResults,
                                           testResult, remainingCords[current+1:][:])
             return None
+
+    def allOrthogonal(self, selectedSet):
+        for cSet in self.allExclusiveCordeSets:
+            #print("testing ", cSet)
+            if selectedSet.isOrthogonal(cSet):
+                yield cSet 
+        
 
         
             
@@ -198,9 +277,38 @@ def fillSet():
         print(x.graphic())
         print(cS.allExclusiveCordeSets[0].overlap(x),"\n\n")
 
+    #print("starting rotation")
+    #firstSet = res[0]
+    #for n in range(firstSet.universe):
+    #    print("rotate by ",n)
+    #print(firstSet.rotate(n).graphic())
 
-    
+    print("starting rotatetall")
+    for x in res[0].rotateAll():
+        print(x.graphic())
+
+    print("starting maxoverlap")
+    print(res[0].graphic())
+    print("\n")
+    for c in res[1:]:
+        print(c.graphic())
+        maxOverlapCSet= res[0].maxOverlap(c)[1]
+        print(maxOverlapCSet.graphic())
+        print(res[0].overlap(maxOverlapCSet))
+
+    print("starting allorthogonal")
+    print(res[5].graphic())
+    for c in cS.allOrthogonal(res[5]):
+        print(c.graphic())
+
+def testAllSizes():
+    cSets = CordeSets(7)
+    res = cSets.allCordeSizes
+    for cS in res:
+        print(cS.graphic())
+        
 if __name__ == '__main__':
     
     #simpletest2()
-    fillSet()
+    #fillSet()
+    testAllSizes()
