bfc468ab080595d211848bd5a0ea00a5da986b50
ib-m-j <ib.m.jorgensen@gmail.com>
2018-09-13 15:39:10 +0200

    prints out howell by rounds and by dealset

--- tournament.py
+++ tournament.py
@@ -22,6 +22,10 @@ class Tournament:
         #dealsets 1..d each dealset consists of  dealsPrSet deals
         self.name = name
         self.rounds = []
+        self.nPairs = 0
+        self.nTables = 0
+        self.nDeals = 0
+        self.nRounds = 0
         
     def __str__(self):
         res = "Tournament: {}".format(self.name)
@@ -42,23 +46,59 @@ class Tournament:
             res = res[:-1]
         res = res+"\n"
         return res
+        
+    def __matrix__(self, tab = 3):
+        table = {}
+        tableRounds = {}
+        for n,r in enumerate(self.rounds):
+            for m in r:
+                table[(m.NSpair,m.EWpair)] = m.dealset
+                tableRounds[(m.NSpair,m.EWpair)] = n
+
+        res = "Tournament: {} - by deals\n".format(self.name)
+        for r in range(self.nRounds):
+            line = ""
+            for p in range(self.nPairs):
+                if (r,p) in table:
+                    line = line + "{:3}".format(table[r,p])
+                else:
+                    line = line + "{:3}".format("")
+            res = res + line + "\n"
+
+        res = res + "Tournament: {} - by rounds\n".format(self.name)
+        for r in range(self.nRounds):
+            line = ""
+            for p in range(self.nPairs):
+                if (r,p) in tableRounds:
+                    line = line + "{:3}".format(tableRounds[r,p])
+                else:
+                    line = line + "{:3}".format("")
+            res = res + line + "\n"
+        return res
+        
+
 
 class GeneratedHowell(Tournament):
-    def __init__(self, name, roundGenerator, dealGenerator):
-        self.roundGenerator = roundGenerator
-        self.dealGenerator = dealGenerator
+    def __init__(self, name, nPairs, nRounds = 0, nDeals = 0):
         Tournament.__init__(self, name)
-        self.nRounds = roundGenerator.universe + 2
-        self.nDeals = dealGenerator.universe + 2
-        assert self.nRounds == self.nDeals, "pairs and deals do not match"
+        seeds = self.getAllHowellSeeds(nPairs - 1)
+        self.roundGenerator = seeds[0][0]
+        self.dealGenerator = seeds[0][1]
+        self.nPairs = nPairs
+        self.nTables = self.nPairs/2
+        self.nDeals = nPairs - 1
+        self.nRounds = nPairs - 1
+        assert nPairs == self.roundGenerator.universe + 1, "mismatch in nPairs"
+
         nDealSet = 0
         deals = {}
         for cSDeal in self.dealGenerator.rotateAll():
             for corde in cSDeal:
                 deals[corde.normalized] = nDealSet
-            deals[(cSDeal.unMatched()[0], cSDeal.universe + 1)] = nDealSet 
+            deals[(cSDeal.unMatched()[0], self.nPairs - 1)] = nDealSet 
             nDealSet = nDealSet + 1
-        print(deals)
+        for d in sorted(deals.keys()):
+            print(d, deals[d])
 
         for cS in self.roundGenerator.rotateAll():
             round = []
@@ -66,15 +106,17 @@ class GeneratedHowell(Tournament):
                 round.append(Match(
                     corde.normalized[0], corde.normalized[1], 
                     deals[corde.normalized]))
+            print(cS.unMatched()[0], self.nPairs - 1, deals[
+                cS.unMatched()[0], self.nPairs - 1])
             round.append(Match(
-                cSDeal.unMatched()[0], cSDeal.universe + 1,
-                deals[corde.normalized]))
+                cS.unMatched()[0], self.nPairs - 1,
+                deals[(cS.unMatched()[0], self.nPairs - 1)]))
 
             self.rounds.append(round)
                 
 
     @classmethod
-    def getAllSeedPairs(self, n):
+    def getAllHowellSeeds(self, n):
         cSets = CordeSets(n)
         res = cSets.allCordeSizes
         orthogonal = []
@@ -91,9 +133,8 @@ def testTournament():
     #    print(pair[0].graphic())
     #    print(pair[1].graphic())
     #    print("--------------------\n")
-    orthogonal = GeneratedHowell.getAllSeedPairs(7)
-    T = GeneratedHowell("Noname", orthogonal[0][0], orthogonal[0][1])
-    print(T)
+    T = GeneratedHowell("Noname", 12)
+    print(T.__matrix__())
 
 if __name__ == '__main__':
     testTournament()
